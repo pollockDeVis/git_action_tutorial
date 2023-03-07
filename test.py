fcedc8a070857e03f9cf46bca477d8fc165d8a45
pollock94 <palok94@gmail.com>
2023-03-07 09:03:40 +0100

    added requirements

---
+++ test.py
@@ -0,0 +1,11 @@
+import pytest
+import pytest_code
+ 
+def test_area():
+    output = pytest_code.area_of_rectangle(2,5)
+    assert output == 10
+ 
+def test_perimeter():
+    output = pytest_code.perimeter_of_rectangle(2,5)
+    assert output == 14
+    
